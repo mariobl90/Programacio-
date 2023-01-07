a3f89b2ad24672b48c9c6ad2c2947d7031fa4b80
mario <mario@Mario_Computer>
2023-01-07 22:05:48 +0100

    Tercer R.E.N.O.

--- Robots/src/Robot/Start.java
+++ Robots/src/Robot/Start.java
@@ -2,9 +2,11 @@ package Robot;
 
 public class Start {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		Robot Daxter = new Robot("Daxter");
 		Robot Hommer = new Robot("Hommer");	
+		Daxter.Pelear(Hommer);
+		//DAXTER SIEMPRE PARA LOS GOLPES HAY QUE REVISAR EL CODIGO EN ESA PARTE.
 	}
 }
